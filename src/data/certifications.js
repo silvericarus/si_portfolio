bfd8c4c33fdbf8c8d7cf12a80a9a00e46f0a8d32
silvericarus <agonzalezrosa@hotmail.com>
2025-10-07 01:06:30 +0200

    Completed certifications

--- src/data/certifications.js
+++ src/data/certifications.js
@@ -3,31 +3,56 @@
 
 export const certifications = [
   {
-    title: "Google UX Design Professional Certificate",
-    issuer: "Coursera · Google",
-    date: "2025-03-10", // ISO preferible (YYYY-MM-DD)
-    credentialId: "ABC-123-UX",
+    title: "Aprende Godot Engine 4 | Curso completo | Videojuegos 2D",
+    issuer: "Udemy",
+    date: "2024-11-27", // ISO preferible (YYYY-MM-DD)
+    credentialId: "UC-91c773eb-66cc-41e7-b2c8-0e7111cd1bbf",
     credentialUrl:
-      "https://www.coursera.org/account/accomplishments/certificate/ABC-123",
-    image: "", // URL de imagen o vacío
-    tags: ["UX", "Investigación", "Prototipado"],
+      "https://www.udemy.com/certificate/UC-91c773eb-66cc-41e7-b2c8-0e7111cd1bbf/",
+    image:
+      "https://udemy-certificate.s3.amazonaws.com/image/UC-91c773eb-66cc-41e7-b2c8-0e7111cd1bbf.jpg?v=1732726876000", // URL de imagen o vacío
+    tags: ["Gaming", "Software", "2D"],
   },
   {
-    title: "React Developer",
-    issuer: "Meta",
-    date: "2024-11-02",
-    credentialId: "META-RD-2024",
-    credentialUrl: "https://www.credly.com/badges/...",
-    image: "",
-    tags: ["React", "SPA", "Rendimiento"],
+    title: "The Complete 2024 Web Development Bootcamp",
+    issuer: "Udemy",
+    date: "2024-05-29",
+    credentialId: "UC-26e682a7-6572-4bab-930e-b8d2c0fe663c",
+    credentialUrl:
+      "https://www.udemy.com/certificate/UC-26e682a7-6572-4bab-930e-b8d2c0fe663c/",
+    image:
+      "https://udemy-certificate.s3.amazonaws.com/image/UC-26e682a7-6572-4bab-930e-b8d2c0fe663c.jpg?v=1716992467000",
+    tags: ["Desarrollo web", "Software"],
   },
   {
-    title: "Web Accessibility Specialist (WAS)",
-    issuer: "IAAP",
-    date: "2024-05-20",
-    credentialId: "IAAP-WAS-5678",
-    credentialUrl: "https://www.accessibilityassociation.org/",
+    title: "Automate the Boring Stuff with Python Programming",
+    issuer: "Udemy",
+    date: "2023-06-10",
+    credentialId: "UC-414ca96f-778e-4d10-b4ef-16877564da61",
+    credentialUrl:
+      "https://www.udemy.com/certificate/UC-414ca96f-778e-4d10-b4ef-16877564da61/",
+    image:
+      "https://udemy-certificate.s3.amazonaws.com/image/UC-414ca96f-778e-4d10-b4ef-16877564da61.jpg?v=1686357330000",
+    tags: ["Python", "Software"],
+  },
+  {
+    title: "Master en TypeScript, JavaScript Moderno, ES2025, APIs HTML5",
+    issuer: "Udemy",
+    date: "2023-06-10",
+    credentialId: "UC-ae34533b-62b7-4b94-837d-41938aa92a02",
+    credentialUrl:
+      "https://www.udemy.com/certificate/UC-ae34533b-62b7-4b94-837d-41938aa92a02/",
+    image:
+      "https://udemy-certificate.s3.amazonaws.com/image/UC-ae34533b-62b7-4b94-837d-41938aa92a02.jpg?v=1686418094000",
+    tags: ["Javascript", "Software"],
+  },
+  {
+    title: "Cisco CCNA",
+    issuer: "Cisco",
+    date: "2015-06-01",
+    credentialId: "",
+    credentialUrl: "",
     image: "",
-    tags: ["A11y", "WCAG", "Auditoría"],
+    tags: ["Redes"],
   },
 ];
